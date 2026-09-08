52126d79d18cb19397292c7d414850b45e46b8f0
maxmax187 <max.tebrake3@gmail.com>
2026-09-08 23:05:48 +0200

    Boss TP: switched vector2 list for GameObject transform list
    also added some more in-editor info-on-hover for BossTeleport script
    -added highlighting for existing teleport locations that are added to script (and order-indicator)

--- HabitGame/Assets/Scripts/Boss/BossTeleport.cs
+++ HabitGame/Assets/Scripts/Boss/BossTeleport.cs
@@ -8,8 +8,16 @@ using UnityEngine;
 
 public class BossTeleport : MonoBehaviour
 {
-    [SerializeField] private Vector2[] _teleportLocations;
+    [Tooltip("Master on/off switch for this pattern. The boss will not teleport while this is disabled, even if locations are configured below.")]
+    [SerializeField] private bool _teleportEnabled;
+
+    [Tooltip("Empty GameObjects placed in the scene marking where the boss can teleport to. Position them visually in the Scene view, then drag their transforms in here.")]
+    [SerializeField] private Transform[] _teleportLocations;
+
+    [Tooltip("Time in seconds between each teleport.")]
     [SerializeField] private float _teleportInterval = 5f;
+
+    [Tooltip("When enabled, locations are chosen at random. When disabled, locations are visited in the order listed above.")]
     [SerializeField] private bool _randomOrder;
 
     private Coroutine _teleportCoroutine;
@@ -17,7 +25,7 @@ public class BossTeleport : MonoBehaviour
 
     public void StartTeleporting()
     {
-        if (_teleportLocations == null || _teleportLocations.Length == 0)
+        if (!_teleportEnabled || _teleportLocations == null || _teleportLocations.Length == 0)
         {
             return;
         }
@@ -49,12 +57,36 @@ public class BossTeleport : MonoBehaviour
     private void TeleportToNextLocation()
     {
         int index = _randomOrder ? Random.Range(0, _teleportLocations.Length) : _nextLocationIndex;
-        transform.localPosition = _teleportLocations[index];
+        transform.position = _teleportLocations[index].position;
 
         _nextLocationIndex++;
         if (_nextLocationIndex >= _teleportLocations.Length)
         {
             _nextLocationIndex = 0;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (_teleportLocations == null)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.cyan;
+        for (int i = 0; i < _teleportLocations.Length; i++)
+        {
+            if (_teleportLocations[i] == null)
+            {
+                continue;
+            }
+
+            Gizmos.DrawWireSphere(_teleportLocations[i].position, 0.2f);
+
+            if (!_randomOrder && i < _teleportLocations.Length - 1 && _teleportLocations[i + 1] != null)
+            {
+                Gizmos.DrawLine(_teleportLocations[i].position, _teleportLocations[i + 1].position);
+            }
         }
     }
 }
